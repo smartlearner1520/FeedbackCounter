166dbb5180a19dc80384bb4afeae40f5815da4dd
Norman Lee <lee.tiong.seng.norman@gmail.com>
2018-07-16 17:08:52 +0800

    Fixed colours on main activity

---
+++ app/src/main/java/com/example/yanglei/feedback/FinalThankYou.java
@@ -0,0 +1,13 @@
+package com.example.yanglei.feedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalThankYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_thank_you);
+    }
+}
